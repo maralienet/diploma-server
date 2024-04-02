701778aaba3c1199ea3f690ac7651e08c697a319
Sophia Bal <foxsophie1705@gmail.com>
2024-04-02 14:46:13 +0300

    git commit

--- client/src/Components/CarsSelect.jsx
+++ client/src/Components/CarsSelect.jsx
@@ -4,12 +4,18 @@ function CarsSelect() {
     return (
         <div className="carSlct">
             <fieldset>
-                <label for='Volvo7456'>Volvo FH16 (AX 7456-7)</label>
-                <input type="radio" name='car' id='Volvo7456'/>
-                <label for='MBenz3021'>Mercedes-Benz Actros (MS 3021-7)</label>
-                <input type="radio" name='car' id='MBenz3021'/>
-                <label for='Scania8790'>Scania R-series (SP 8790-7)</label>
-                <input type="radio" name='car' id='Scania8790'/>
+                <div className="input_type__radio">
+                    <input type="radio" name='car' id='Volvo7456' />
+                    <label for='Volvo7456'>Volvo FH16 (AX 7456-7)</label>
+                </div>
+                <div className="input_type__radio">
+                    <input type="radio" name='car' id='MBenz3021' />
+                    <label for='MBenz3021'>Mercedes-Benz Actros (MS 3021-7)</label>
+                </div>
+                <div className="input_type__radio">
+                    <input type="radio" name='car' id='Scania8790' />
+                    <label for='Scania8790'>Scania R-series (SP 8790-7)</label>
+                </div>
             </fieldset>
         </div>
     );
