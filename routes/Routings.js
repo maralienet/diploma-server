968d1bedba0d2f6092f4bf5b33385badc701210c
Sophia Bal <foxsophie1705@gmail.com>
2024-04-14 12:05:24 +0300

    git commit

--- routes/Routings.js
+++ routes/Routings.js
@@ -13,6 +13,34 @@ router.get('/', async (req, res) => {
     res.json(RoutingsList);
 });
 
+router.get("/bycar", async (req, res) => {
+    const [result, metadata] = await Routings.sequelize.query(`
+    select carId,count(*) as routesCount,sum(length) as totalLength 
+    from routings
+    group by carId;
+    `);
+    res.json(result);
+});
+
+router.get("/bycar/:id", async (req, res) => {
+    const { id } = req.params;
+    const [result, metadata] = await Routings.sequelize.query(`
+    select carId,count(*) as routesCount,sum(length) as totalLength 
+    from routings
+    where carId=${id}
+    group by carId;
+    `);
+    res.json(result);
+});
+
+router.get("/byroute", async (req, res) => {
+    const [result, metadata] = await Routings.sequelize.query(`
+    select routeId,group_concat(carId) as cars,route,length
+    from routings
+    group by routeId,route,length;
+    `);
+    res.json(result);
+});
 
 router.post('/', async (req, res) => {
     const route = req.body;
