e64cef4c6aca2021d1a44b9a62ceb85777c1f698
Sophia Bal <foxsophie1705@gmail.com>
2024-05-04 17:59:10 +0300

    git commit

--- routes/Users.js
+++ routes/Users.js
@@ -1,42 +1,52 @@
 const express = require('express');
-const Sequelize = require('sequelize');
 const router = express.Router();
-
-const { Users } = require('../models');
+const db = require('../db');
 
 router.get("/", async (req, res) => {
     let query = req.query;
-    let listOfUsers;
-
+    let users;
     if (Object.keys(query).length === 0)
-        listOfUsers = await Users.findAll({});
-    else
-        listOfUsers = await Users.findAll({
-            where: query
-        });
-    res.json(listOfUsers);
+        users = await db.query('select * from users');
+    else {
+        let queryParams = [];
+        let queryText = 'select * from users where ';
+        let i = 1;
+        for (let key in query) {
+            queryText += `${key} = $${i} and `;
+            queryParams.push(query[key]);
+            i++;
+        }
+        queryText = queryText.slice(0, -5);
+
+        users = await db.query(queryText, queryParams);
+    }
+    res.json(users.rows);
 });
 
-
 router.get("/maxid", async (req, res) => {
-    const userWithMaxId = await Users.findOne({ order: [['id', 'DESC']] });
+    const userWithMaxId = await db.query(`
+    select * from users 
+    where id=(select max(id) from users) 
+	order by id desc limit 1
+    `);
     res.json(userWithMaxId);
 });
 
 router.post("/", async (req, res) => {
-    const user = req.body;
-    const createdUser = await Users.create(user);
+    const { name, surname, login, password, role } = req.body;
+    const createdAt = new Date().toJSON();
+    const createdUser = await db.query(`insert into users values ()`);
     res.json(createdUser);
 });
 
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, surname, role } = req.body;
-    const item = await Users.findByPk(id);
+// router.put('/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const { name, surname, role } = req.body;
+//     const item = await Users.findByPk(id);
 
-    const updatedItem = await item.update({ name, surname, role });
-    return res.json(updatedItem);
-});
+//     const updatedItem = await item.update({ name, surname, role });
+//     return res.json(updatedItem);
+// });
 
 
 module.exports = router;
