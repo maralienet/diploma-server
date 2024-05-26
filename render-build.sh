81a40cf7052c3a99042d506db4057fa01cc6a188
maralienet <maralienet@gmail.com>
2024-05-26 19:42:23 +0300

    git commit

--- render-build.sh
+++ render-build.sh
@@ -12,7 +12,11 @@ npm install
 # Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  if [[ -d $XDG_CACHE_HOME/puppeteer/ ]]; then
+    cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  else
+    echo "Directory $XDG_CACHE_HOME/puppeteer/ does not exist"
+  fi
 else 
   echo "...Storing Puppeteer Cache in Build Cache" 
   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
